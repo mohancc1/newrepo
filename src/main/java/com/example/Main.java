5e65a8654cf317f07ad8f5b000c1068ac28e7ba1
mohancc1 <mohanccdba@gmail.com>
2025-07-12 00:11:12 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,6 @@
-package com.example;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from myapp!");
+        Thread.sleep(Long.MAX_VALUE); // keeps app running
     }
 }
